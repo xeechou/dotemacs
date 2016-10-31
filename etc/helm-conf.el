07d03744a8dec27e00c8f7cca343a50ae52acab3
Sichem Zhou <xeechou@gmail.com>
2016-10-30 20:53:07 -0400

    switch to helm-gtags

--- etc/helm-conf.el
+++ etc/helm-conf.el
@@ -99,3 +99,35 @@
 ;(define-key global-map [remap list-buffers] 'helm-buffers-list)
 
 (helm-mode 1)
+
+
+
+
+;;;;;;;;;;;;Now, we setup the helm-gtags;;;;ggtags itself is bit hard to use now
+(setq
+ helm-gtags-ignore-case t
+ helm-gtags-auto-update t
+ helm-gtags-use-input-at-cursor t
+ helm-gtags-pulse-at-cursor t
+ helm-gtags-prefix-key "\C-cg"
+ helm-gtags-suggested-key-mapping t
+ )
+
+(require 'helm-gtags)
+;;enable helm-gtags mode
+(add-hook 'dired-mode-hook 'helm-gtags-mode)
+(add-hook 'eshell-mode-hook 'helm-gtags-mode)
+(add-hook 'c-mode-hook 'helm-gtags-mode)
+(add-hook 'c++-mode-hook 'helm-gtags-mode)
+(add-hook 'asm-mode-hook 'helm-gtags-mode)
+
+(define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+;;hel-gtags-select show all the tags in current project
+(define-key helm-gtags-mode-map (kbd "C-c g A") 'helm-gtags-select)
+(define-key helm-gtags-mode-map (kbd "C-c g f") 'helm-gtags-find-files)
+(define-key helm-gtags-mode-map (kbd "C-c g r") 'helm-gtags-find-rtag)
+(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+;;(define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+;;(define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+
