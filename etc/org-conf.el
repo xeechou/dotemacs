7330654cb3dc4cda4bb97e2aab552ad061ff2bf2
Sichem Zhou <xeechou@gmail.com>
2016-11-26 06:16:16 -0500

    agenda

--- etc/org-conf.el
+++ etc/org-conf.el
@@ -10,9 +10,22 @@
 	  ("DONE" . org-level-5)
 	  ("CANC" . org-level-4)
 	  ("PEND" . org-level-3)))
-  
+  (setq org-clock-persist 'history)
+  (org-clock-persistence-insinuate)
+
+  (setq org-default-notes-file (concat org-directory "/notes.org"))
+;  (define-key global-map "\C-cc" 'org-capture)
+
+  ;I am not sure this global key setting is good or not, capture stuff globally is great
+  (global-set-key "\C-ca" 'org-agenda)
+  (global-set-key "\C-cc" 'org-capture)
+  (global-set-key "\C-cb" 'org-iswitchb)
   :config
   (progn
+    (setq org-agenda-files (list "~/org/work.org"
+				 "~/org/training.org"
+				 "~/org/miscs.org"
+				 "~/org/today.org"))
     (setq org-log-done t)
     (setq org-agenda-files (list "~/org/work.org"
 				 "~/org/training.org"
