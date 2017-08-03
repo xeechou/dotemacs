3665e41a190d38a0cc1e1bdbcea495a815cf03b3
Sichem Zhou <xeechou@gmail.com>
2017-08-03 10:46:36 -0400

    cool

--- init.el
+++ init.el
@@ -49,6 +49,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(irony-additional-clang-options (quote ("-std=c++11")))
  '(package-selected-packages
    (quote
     (yasnippet irony company-irony-c-headers glsl-mode paganini-theme use-package zenburn-theme rust-mode org irony-eldoc helm-gtags ggtags flycheck-rust flycheck-irony diminish company-jedi company-irony bind-key atom-one-dark-theme atom-dark-theme))))
