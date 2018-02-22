c39614625dcc27f11cf22692d456dbb33881dbcb
xeechou <xeechou@gmail.com>
2018-02-22 17:27:56 -0500

    sadfklj

--- etc/languages.el
+++ etc/languages.el
@@ -44,10 +44,6 @@
 
 
 ;; company
-;;I may replace this with rtags in the future
-
-
-
 (use-package  company
   :ensure t
   :defer t
@@ -61,7 +57,44 @@
   (add-hook 'js-mode-hook  'company-mode)
   (add-hook 'lua-mode-hook 'company-mode)
   :config
-  ;;irony-backend
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; general setup ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (setq company-minimum-prefix-length 2
+	company-idle-delay 0.1
+	company-async-timeout 10
+	company-backends  '((company-files
+			     company-keywords
+			     company-yasnippet))
+	)
+
+  (defun complete-or-indent ()
+    (interactive)
+    (if (company-manual-begin)
+	(company-complete-common)
+      (indent-according-to-mode)))
+  (defun indent-or-complete ()
+    (interactive)
+    (if (looking-at "\\_>")
+	(company-complete-common)
+      (indent-according-to-mode)))
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;setting up flycheck;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (use-package flycheck
+    :ensure t
+    :diminish flycheck-mode
+    :init
+    (add-hook 'c-mode-hook 'flycheck-mode)
+    (add-hook 'c++-mode-hook 'flycheck-mode)
+    (add-hook 'python-mode-hook 'flycheck-mode)
+    :config
+    (when (equal major-mode 'c++-mode)
+      (setq flycheck-clang-language-standard "c++14"))
+    (when (equal major-mode 'c-mode)
+      (setq flycheck-clang-language-standard "c11"))
+
+    (use-package flycheck-irony :ensure t)
+    (add-hook 'flycheck-mode-hook #'flycheck-irony-setup)
+    )
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; C++ setup ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (use-package irony
     :ensure t
     :defer t
@@ -101,91 +134,57 @@
     (use-package irony-eldoc
       :ensure t
       :config (add-hook 'irony-mode-hook 'irony-eldoc))
+    (use-package company-irony :ensure t :defer t)
+    (use-package company-irony-c-headers :ensure t :defer t)
+    ;;;;;setup the Rtags as well in the irony
+    (if (not (string-equal system-type "windows-nt"))
+	(use-package rtags
+	  :ensure t
+	  :defer  t
+	  :init
+	  (unless (file-exists-p "/usr/bin/rdm")
+	    (setq rtags-path (concat (getenv "HOME") "/.bin/"))
+	    )
+	  (defun my-close-rtags-taglist ()
+	    "close rtags-tagslist when in the taglist"
+	    (interactive)
+	    (windmove-right)
+	    (rtags-close-taglist)
+	    )
+	  (add-hook 'c-mode-hook 'rtags-start-process-unless-running)
+	  (add-hook 'c++-mode-hook 'rtags-start-process-unless-running)
+	  :config
+	  ;;define the keybindings
+	  (define-key rtags-taglist-mode-map (kbd "q") 'my-close-rtags-taglist)
+	  (define-key c-mode-base-map (kbd "C-x t") 'rtags-taglist)
+	  (define-key c-mode-base-map (kbd "M-,") 'rtags-find-references-at-point)
+	  (define-key c-mode-base-map (kbd "M-.") 'rtags-find-symbol-at-point)
+	  (rtags-enable-standard-keybindings)
+					;setup the completion, rtags completion is very ustable, just use irony
+					;(setq rtags-autostart-diagnostics t)
+					;(rtags-diagnostics)
+					;(setq rtags-completions-enabled t)
+					;(define-key c-mode-base-map (kbd "<C-tab>") (function company-complete))
+	  )
+      )
+    )
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; python setup ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (use-package company-jedi
+    :ensure t
+    :defer t
+    :config
+    (use-package jedi-direx :ensure t :defer t
+      :config (add-hook 'jedi-mode-hook 'jedi-direx:setup))
+    :bind (:map python-mode-map
+		("M-," . jedi:goto-definition)
+		("M-." . jedi:goto-definition-next)
+	   )
     )
   ;;other packages
-  (use-package company-irony :ensure t :defer t)
-  (use-package company-irony-c-headers :ensure t :defer t)
   (use-package company-lua   :ensure t :defer t)
-  (use-package company-jedi  :ensure t :defer t)
-
-  (use-package flycheck
-    :ensure t
-    :diminish flycheck-mode
-    :init
-
-    (add-hook 'c-mode-hook 'flycheck-mode)
-    (add-hook 'c++-mode-hook 'flycheck-mode)
-    (add-hook 'python-mode-hook 'flycheck-mode)
-    :config
-    (when (equal major-mode 'c++-mode)
-      (setq flycheck-clang-language-standard "c++14"))
-    (when (equal major-mode 'c-mode)
-      (setq flycheck-clang-language-standard "c11"))
-;;    (setq flycheck-clang-language-standard "c++14")    )
-    ;(use-package flycheck-rtags :ensure t)
-
-    (defun my-flycheck-setup ()
-      (flycheck-select-checker 'rtags)
-      (setq-local flycheck-highlighting-mode nil)
-      (setq-local flycheck-check-syntax-automatically nil)
-      )
-    ;(add-hook 'c-mode-hook 'my-flycheck-setup)
-    ;(add-hook 'c++-mode-hook 'my-flycheck-setup)
-    (use-package flycheck-irony :ensure t)
-    (add-hook 'flycheck-mode-hook #'flycheck-irony-setup)
-
-    )
-  (if (not (string-equal system-type "windows-nt"))
-      (use-package rtags
-	:ensure t
-	:defer  t
-	:init
-	(unless (file-exists-p "/usr/bin/rdm")
-	  (setq rtags-path (concat (getenv "HOME") "/.bin/"))
-	  )
-	(defun my-close-rtags-taglist ()
-	  "close rtags-tagslist when in the taglist"
-	  (interactive)
-	  (windmove-right)
-	  (rtags-close-taglist)
-	  )
-	(add-hook 'c-mode-hook 'rtags-start-process-unless-running)
-	(add-hook 'c++-mode-hook 'rtags-start-process-unless-running)
-	:config
-	;;define the keybindings
-	(define-key rtags-taglist-mode-map (kbd "q") 'my-close-rtags-taglist)
-	(define-key c-mode-base-map (kbd "C-x t") 'rtags-taglist)
-	(define-key c-mode-base-map (kbd "M-,") 'rtags-find-references-at-point)
-	(define-key c-mode-base-map (kbd "M-.") 'rtags-find-symbol-at-point)
-	(rtags-enable-standard-keybindings)
-	;setup the completion, rtags completion is very ustable, just use irony
-	;(setq rtags-autostart-diagnostics t)
-	;(rtags-diagnostics)
-	;(setq rtags-completions-enabled t)
-	;(define-key c-mode-base-map (kbd "<C-tab>") (function company-complete))
-	)
-    )
-  ;; (use-package company-rtags :ensure t :defer t)
-
-  (setq company-minimum-prefix-length 2
-	company-idle-delay 0.1
-	company-async-timeout 10
-	company-backends  '((company-files
-			     company-keywords
-			     company-yasnippet))
-	)
-
-  (defun complete-or-indent ()
-    (interactive)
-    (if (company-manual-begin)
-	(company-complete-common)
-      (indent-according-to-mode)))
-  (defun indent-or-complete ()
-    (interactive)
-    (if (looking-at "\\_>")
-	(company-complete-common)
-      (indent-according-to-mode)))
-
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Backend-setup;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (dolist (hook '(c-mode-hook
 		  c++-mode-hook))
     (add-hook hook
@@ -229,7 +228,10 @@
 ;; glsl
 (use-package glsl-mode
   :ensure t
-  :mode (("\\.glsl\\'" . glsl-mode)))
+  :mode (("\\.glsl\\'" . glsl-mode)
+	 ("\\.vs\\'" . glsl-mode)
+	 ("\\.fs\\'" . glsl-mode)
+	 ("\\.gs\\'" . glsl-mode)))
 (use-package cuda-mode
   :ensure t
   :mode (("\\.cu\\'" . cuda-mode)))
@@ -271,6 +273,7 @@
 (require 'sgml-mode)
 (require 'nxml-mode)
 (add-hook 'c-mode-common-hook   'hs-minor-mode)
+(add-hook 'python-mode-hook     'hs-minor-mode)
 (add-hook 'emacs-lisp-mode-hook 'hs-minor-mode)
 (add-hook 'lisp-mode-hook       'hs-minor-mode)
 (add-hook 'perl-mode-hook       'hs-minor-mode)
@@ -286,15 +289,50 @@
 	       nil))
 (add-hook 'nxml-mode-hook 'hs-minor-mode)
 
+(defun hs-hide-leafs-recursive (minp maxp)
+  "Hide blocks below point that do not contain further blocks in
+    region (MINP MAXP)."
+  (when (hs-find-block-beginning)
+    (setq minp (1+ (point)))
+    (funcall hs-forward-sexp-func 1)
+    (setq maxp (1- (point))))
+  (unless hs-allow-nesting
+    (hs-discard-overlays minp maxp))
+  (goto-char minp)
+  (let ((leaf t))
+    (while (progn
+	     (forward-comment (buffer-size))
+	     (and (< (point) maxp)
+		  (re-search-forward hs-block-start-regexp maxp t)))
+      (setq pos (match-beginning hs-block-start-mdata-select))
+      (if (hs-hide-leafs-recursive minp maxp)
+	  (save-excursion
+	    (goto-char pos)
+	    (hs-hide-block-at-point t)))
+      (setq leaf nil))
+    (goto-char maxp)
+    leaf))
+
+(defun hs-hide-leafs ()
+  "Hide all blocks in the buffer that do not contain subordinate
+    blocks.  The hook `hs-hide-hook' is run; see `run-hooks'."
+  (interactive)
+  (hs-life-goes-on
+   (save-excursion
+     (message "Hiding blocks ...")
+     (save-excursion
+       (goto-char (point-min))
+       (hs-hide-leafs-recursive (point-min) (point-max)))
+     (message "Hiding blocks ... done"))
+   (run-hooks 'hs-hide-hook)))
 
 ;;;Hide/Show minor-mode is a much better mode to work with
 (setq cm-map (make-sparse-keymap))
 (global-set-key "\M-o" cm-map)
 (define-key cm-map "t" 'hs-toggle-hiding)         ; Toggle hiding, which is very useful
 					; HIDE
-(define-key cm-map "a" 'hs-hide-all)    ; Hide everything but the top-level headings
+(define-key cm-map "a" 'hs-hide-leafs)    ; Hide everything but the top-level headings
 (define-key cm-map "c" 'hs-hide-comment-region) ;Hide comment ?
-(define-key cm-map "b" 'hs-hide-block)  ; Hide the block, but you may use toggle-hiding more frequently
-					; SHOW
+(define-key cm-map "l" 'hs-hide-level)
 (define-key cm-map "A" 'hs-show-all)       ; Show (expand) everything
 (define-key cm-map "B" 'hs-show-block)     ; Show this heading's body
