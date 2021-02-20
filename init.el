17d41001a37be162a796024123d92b4cc45a7d16
yijiu <yijiu@qunhemail.com>
2021-02-20 11:30:11 +0800

    using dejavu sans mono for emacs app

--- init.el
+++ init.el
@@ -129,5 +129,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 133 :width normal))))
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "outline" :slant normal :weight normal :height 143 :width normal))))
  '(show-paren-match ((t (:background "yellow" :foreground "gainsboro" :slant oblique :weight ultra-bold)))))
