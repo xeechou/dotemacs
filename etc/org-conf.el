801716f8a490c043b50da55f4f494772ba35d778
xzhou <xzhou@xeechou.com>
2021-02-20 20:12:06 +0800

    using my own org-push org-pull

--- etc/org-conf.el
+++ etc/org-conf.el
@@ -1,4 +1,3 @@
-(use-package org-mobile-sync :ensure t)
 (use-package org-mode
   :ensure org
   :mode (("\\.org$" . org-mode))
@@ -14,7 +13,6 @@
   (setq org-clock-persist 'history)
   (org-clock-persistence-insinuate)
 
-
   ;; recursively update the parents TODO
   (defun org-summary-todo (n-done n-not-done)
     "Switch entry to DONE when all subentries are don, to TODO otherwise"
@@ -23,7 +21,6 @@
   (add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
   ;;setup the default directory
   (setq org-directory "~/org/")
-  (setq org-mobile-inbox-for-pull "~/org/index.org")
   (setq org-mobile-directory "~/Documents/org-remote/")
 
   (setq org-default-notes-file (concat org-directory "miscs.org"))
@@ -38,12 +35,63 @@
 				       (concat org-directory "goals-habits.org")
 				       (concat org-directory "social.org"))))
 
+  ;; org-push
+  (defun org-push-copy ()
+    "copy agenda files"
+    (if (file-exists-p org-mobile-directory)
+	(dolist (src org-mobile-files)
+	  (let (name dest)
+	    (progn
+	      (setq name (file-name-nondirectory src))
+	      (setq dest (concat org-mobile-directory name))
+	      (copy-file src dest 'ok-if-already-exists))
+	    ))
+      (message "%s does not exist" org-mobile-directory))
+    )
+  (defun org-push ()
+    "org push but delete the agendas and mobileorg, use this for now"
+    (interactive)
+    (save-excursion
+      (save-restriction
+	(save-window-excursion
+	  (message "Creating agendas... done")
+	  (org-save-all-org-buffers)
+	  (message "Coping agendas... done") ;;we will last see this if no error happens
+	  (org-push-copy)
+	  )))
+    )
+  ;; org-pull
+  (defun org-pull-copy ()
+    "copy agenda files"
+    (if (file-exists-p org-mobile-directory)
+	(dolist (dest org-mobile-files)
+	  (let (name src)
+	    (progn
+	      (setq name (file-name-nondirectory dest))
+	      (setq src (concat org-mobile-directory name))
+	      (copy-file src dest 'ok-if-already-exists))
+	    ))
+      (message "remote %s does not exist" org-mobile-directory))
+    )
+  (defun org-pull ()
+    "pulling all the agendas from remote, this overrides current files"
+    (interactive)
+    (save-excursion
+      (save-restriction
+	(save-window-excursion
+	  (message "saving agendas... done")
+	  (org-save-all-org-buffers)
+	  (message "pulling agendas...done")
+	  (org-pull-copy)
+	  )))
+    )
   ;I am not sure this global key setting is good or not, capture stuff globally is great
-  (global-set-key "\C-ca" 'org-agenda)
-  (global-set-key "\C-cc" 'org-capture)
-  (global-set-key "\C-cb" 'org-iswitchb)
-
+  :bind (:map global-map
+	      ("\C-ca" . org-agenda)
+	      ("\C-cc" . org-capture)
+	      ("\C-cb" . org-iswitchb))
   :config
+
   (progn
     (setq org-log-done 'time)
     )
