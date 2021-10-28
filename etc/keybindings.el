3c474c413eb9994d52f3e4c40f62f2962ff02406
Xichen Zhou <xeechou@xzhou.net>
2021-10-28 16:47:41 -0400

    add reloading buffer-local variables

--- etc/keybindings.el
+++ etc/keybindings.el
@@ -95,7 +95,6 @@
 ;;****************************E*******************************
 ;;************************************************************
 
-
 (defun my-next-word (p)
   "Move point to the beginning of the next word, past by any space"
   (interactive "d")
@@ -114,3 +113,23 @@
 ;;************************************************************
 ;;************************************************************
 (global-set-key (kbd  "\C-x r i") 'string-insert-rectangle)
+
+;;************************************************************
+;;************************************************************
+;;************************************************************
+;; reload dir-locals.el
+(defun reload-dir-locals-for-current-buffer ()
+  "reload dir locals for the current buffer"
+  (interactive)
+  (let ((enable-local-variables :all))
+    (hack-dir-local-variables-non-file-buffer)))
+
+(defun reload-dir-locals-for-all-buffer-in-this-directory ()
+  "For every buffer with the same `default-directory` as the
+current buffer's, reload dir-locals."
+  (interactive)
+  (let ((dir default-directory))
+    (dolist (buffer (buffer-list))
+      (with-current-buffer buffer
+        (when (equal default-directory dir)
+          (my-reload-dir-locals-for-current-buffer))))))
