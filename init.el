1760ac226dff8059ae983759725fc322226a3417
Xichen Zhou <xeechou@xzhou.net>
2023-10-17 10:05:24 -0400

    make custom file out of init

--- init.el
+++ init.el
@@ -1,3 +1,14 @@
+;;make set-custom-variable elsewhere
+(customize-save-variable 'custom-file (expand-file-name "custom.el" user-emacs-directory))
+(unless (file-exists-p custom-file)
+  (write-region "(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )" nil custom-file))
+
+
 (display-time)
 ;; 1) this setting avoids subdirectory do not starts with letter or digit
 (add-to-list 'load-path "~/.emacs.d/lisp")
@@ -77,46 +88,5 @@
 (when (eq system-type 'windows-nt) ;;use it only for windows now
   (load-env-paths "^.+_BINPATH="))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("afa47084cb0beb684281f480aa84dab7c9170b084423c7f87ba755b15f6776ef" "e1f4f0158cd5a01a9d96f1f7cdcca8d6724d7d33267623cc433fe1c196848554" "70b596389eac21ab7f6f7eb1cf60f8e60ad7c34ead1f0244a577b1810e87e58c" default))
- '(lsp-log-io nil nil nil "Customized with use-package lsp-mode")
- '(package-selected-packages
-   '(org-journal company-glsl hlsl-mode azsl-mode uuidgen json-mode rmsbolt treesit-auto doom-themes web-mode org-modern org-roam-ui simple-httpd projectile yasnippet-snippets smart-tabs-mode company-c-headers visual-fill-column whitespace-cleanup-mode which-key format-all magit org-contrib org-ref ivy-bibtex org-download org-cliplink org-roam mixed-pitch gdscript-mode graphviz-dot-mode meson-mode rjsx-mode shader-mode glsl-mode cmake-mode python-mode backward-forward smex counsel-tramp flyspell-correct-popup flyspell-correct company-emoji exec-path-from-shell yaml-mode websocket vc-use-package unity typescript-mode ssh-agency spacemacs-theme rust-mode openwith modus-themes markdown-mode lua-mode ligature go-mode fic-mode diminish dash dart-mode counsel color-rg))
- '(package-vc-selected-packages
-   '((hlsl-mode :vc-backend Git :url "https://github.com/xeechou/hlsl-mode.el")
-     (azsl-mode :vc-backend Git :url "https://github.com/xeechou/azsl-mode.el")
-     (ligature :vc-backend Git :url "https://github.com/mickeynp/ligature.el")))
- '(safe-local-variable-values
-   '((org-image-actual-width)
-     (projectile-compilation-dir . "./build/ninja"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-block ((t (:inherit fixed-pitch))))
- '(org-code ((t (:inherit (shadow fixed-pitch)))))
- '(org-document-info ((t (:foreground "dark orange"))))
- '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
- '(org-document-title ((t (:inherit default :weight bold :foreground "unspecified-fg" :underline nil))))
- '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
- '(org-level-1 ((t (:inherit default :weight bold :foreground "unspecified-fg" :background unspecified :height 2.0))))
- '(org-level-2 ((t (:inherit default :weight bold :foreground "unspecified-fg" :background unspecified :height 1.5))))
- '(org-level-3 ((t (:inherit default :weight bold :foreground "unspecified-fg" :background unspecified :height 1.25))))
- '(org-level-4 ((t (:inherit default :weight bold :foreground "unspecified-fg" :background unspecified :height 1.1))))
- '(org-level-5 ((t (:inherit default :weight bold :foreground "unspecified-fg"))))
- '(org-level-6 ((t (:inherit default :weight bold :foreground "unspecified-fg"))))
- '(org-level-7 ((t (:inherit default :weight bold :foreground "unspecified-fg"))))
- '(org-level-8 ((t (:inherit default :weight bold :foreground "unspecified-fg"))))
- '(org-link ((t (:foreground "royal blue" :underline t))))
- '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-property-value ((t (:inherit fixed-pitch))) t)
- '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
- '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
- '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
+;;;;;;;;;;;; finally load user config ;;;;;;;;;;
+(load custom-file)
