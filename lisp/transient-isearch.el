dd4ae3dd7f546dedf34ea49f202d51ce2ac18c0d
Xichen Zhou <xeechou@xzhou.net>
2025-07-05 09:29:09 -0400

    isearch updates
    makes isearch more convenient
    
    Signed-off-by: Xichen Zhou <xeechou@xzhou.net>

---
+++ lisp/transient-isearch.el
@@ -0,0 +1,108 @@
+;;; transient-isearch.el --- improve isearch function with transient -*- lexical-binding: t
+;;; -*-
+
+;; Copyright (C) 2023 Charles Choi
+;; Copyright (C) 2024 Xichen Zhou
+;; Author: Xichen Zhou <sichem.zh@gmail.com>
+;; Maintainer: Xichen Zhou <sichem.zh@gmail.com>
+
+;; Package-Requires: ((emacs "29.1") (transient "0.7"))
+;; Version: 0.0.1
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This package simple provides a transient menu for isearch
+;;; Code:
+
+(require 'transient)
+
+(transient-define-prefix transient-isearch-menu ()
+  "isearch Menu"
+  [["Edit Search String"
+    ("e"
+     "Edit the search string (recursive)"
+     isearch-edit-string
+     :transient nil)
+    ("w"
+     "Pull next word or character word from buffer"
+     isearch-yank-word-or-char
+     :transient nil)
+    ("s"
+     "Pull next symbol or character from buffer"
+     isearch-yank-symbol-or-char
+     :transient nil)
+    ("l"
+     "Pull rest of line from buffer"
+     isearch-yank-line
+     :transient nil)
+    ("y"
+     "Pull string from kill ring"
+     isearch-yank-kill
+     :transient nil)
+    ("t"
+     "Pull thing from buffer"
+     isearch-forward-thing-at-point
+     :transient nil)]
+
+   ["Replace"
+    ("q"
+     "Start ‘query-replace’"
+     isearch-query-replace
+     :if-nil buffer-read-only
+     :transient nil)
+    ("x"
+     "Start ‘query-replace-regexp’"
+     isearch-query-replace-regexp
+     :if-nil buffer-read-only
+     :transient nil)]]
+
+  [["Toggle"
+    ("X"
+     "Toggle regexp searching"
+     isearch-toggle-regexp
+     :transient nil)
+    ("S"
+     "Toggle symbol searching"
+     isearch-toggle-symbol
+     :transient nil)
+    ("W"
+     "Toggle word searching"
+     isearch-toggle-word
+     :transient nil)
+    ("F"
+     "Toggle case fold"
+     isearch-toggle-case-fold
+     :transient nil)
+    ("L"
+     "Toggle lax whitespace"
+     isearch-toggle-lax-whitespace
+     :transient nil)]
+
+   ["Misc"
+    ("o"
+     "occur"
+     isearch-occur
+     :transient nil)]])
+
+;;;###autoload
+(defun transient-isearch-open-menu ()
+  "Open the transient isearch menu"
+  (interative)
+  (transient-isearch-menu))
+
+(provide 'transient-isearch)
